6fe057ebaec587c541fbe235f131c8712d4db926
Djeen51 <djeen51@gmail.com>
2023-08-11 06:41:23 +0200

    nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from './pages/AppLayout';
+import Login from "./pages/Login";
+import AppNav from "./components/AppNav";
 
 
 function App() {
@@ -13,7 +16,14 @@ function App() {
           <Route path="/" element ={<Homepage />} />
           <Route path="/product" element ={<Product/>}/>
           <Route path="/pricing" element ={<Pricing/>} />
+          <Route path="/login" element ={<Login/>} />
           <Route path="*" element={<PageNotFound/>} />
+          <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>Default</p>}/>
+            <Route path="cities" element={<p>list of cities</p>}/>
+            <Route path="countries" element={<p>Countries</p>}/>
+            <Route path="form" element={<p>form</p>} />
+          </Route>
         </Routes>
       </BrowserRouter>
   )
